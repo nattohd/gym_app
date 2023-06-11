108b8c18402f68d6c19caef067ab81c329d5c4b0
Cristobal-code <cristobalh517@gmail.com>
2023-06-11 18:11:24 -0400

    adding interactuable grilla

--- lib/src/screens/screens.dart
+++ lib/src/screens/screens.dart
@@ -2,3 +2,5 @@ export 'package:gym_app/src/screens/user/login_screen.dart';
 export 'package:gym_app/src/screens/user/profile_screen.dart';
 export 'package:gym_app/src/screens/home/home_screen.dart';
 export 'package:gym_app/src/screens/reservas/reservas_screen.dart';
+export 'package:gym_app/src/screens/qr/qrgenerate_screen.dart';
+export 'package:gym_app/src/screens/qr/scanqr_screen.dart';
