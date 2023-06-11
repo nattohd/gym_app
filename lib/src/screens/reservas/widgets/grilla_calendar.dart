afcc85dd17cbd3fa435be727e9a763fc78beca48
XManGHX <francoman71@gmail.com>
2023-06-11 18:34:06 -0400

    conflict change

--- lib/src/screens/reservas/widgets/grilla_calendar.dart
+++ lib/src/screens/reservas/widgets/grilla_calendar.dart
@@ -11,20 +11,26 @@ class GrillaCalendar extends StatelessWidget {
     List<int> columnas = List<int>.generate(8, (index) => index);
     Size size = MediaQuery.of(context).size;
     final colors = Theme.of(context).colorScheme;
+
     return Column(
       children: columnas.map((i) {
         List<int> filas = List<int>.generate(5, (index) => index);
         return Row(
           children: filas.map((i) {
-            return Container(
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: colors.primary,
-                  width: .1,
+            return InkWell(
+              splashColor: colors.primary.withOpacity(.1),
+              hoverColor: colors.primary.withOpacity(.1),
+              onTap: () {},
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: colors.primary,
+                    width: .1,
+                  ),
                 ),
+                width: size.width * 0.85 / 5,
+                height: heightGrilla,
               ),
-              width: size.width * 0.85 / 5,
-              height: heightGrilla,
             );
           }).toList(),
         );
