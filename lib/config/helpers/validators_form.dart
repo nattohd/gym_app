5d2e4d5f2201b388b8cd728cba7de75a7e3114f7
Cristobal-code <cristobalh517@gmail.com>
2023-06-13 19:18:19 -0400

    Fixing validatorForm

--- lib/config/helpers/validators_form.dart
+++ lib/config/helpers/validators_form.dart
@@ -1,20 +1,40 @@
+class PasswordErrors {
+  static const String isEmpty = 'Debe ingresar una contraseña';
+  static const String isLeastLength =
+      'La contraseña debe ser mayor a 8 caracteres';
+  static const String isSpecialCaracters =
+      'La contraseña debe contener al menos un caracter especial.';
+
+  PasswordErrors();
+}
+
 class ValidatorsForm {
   static bool isValidEmail(value) {
     return RegExp(r"^[a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@(usm|sansano)\.cl$")
         .hasMatch(value);
   }
 
-  static dynamic isCorrectFormatPassword(String value) {
-    if (value.isEmpty) {
-      return 'Debe ingresar una contraseña';
+  static bool isPasswordDefined(String value) {
+    return value.isEmpty;
+  }
+
+  static List<String> isCorrectFormatPassword(String value) {
+    final errors = [
+      PasswordErrors.isEmpty,
+      PasswordErrors.isLeastLength,
+      PasswordErrors.isSpecialCaracters,
+    ];
+
+    if (value.isNotEmpty) {
+      errors.remove(PasswordErrors.isEmpty);
     }
-    if (value.length < 8) {
-      return 'La contraseña debe ser mayor a 8 caracteres';
+    if (value.length >= 8) {
+      errors.remove(PasswordErrors.isLeastLength);
     }
     RegExp regexCaractSpecial = RegExp(r'[!@#$%^&*(),.?":{}|<>]');
     if (!regexCaractSpecial.hasMatch(value)) {
-      return 'La contraseña debe contener al menos un caracter especial.';
+      errors.remove(PasswordErrors.isSpecialCaracters);
     }
-    return true;
+    return errors;
   }
 }
